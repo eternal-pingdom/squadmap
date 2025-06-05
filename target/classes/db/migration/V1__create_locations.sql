03cb8370acbae49d43d585ef2c208949aa20b46e
Ob-Ivan <55740240+0b-ivan@users.noreply.github.com>
2025-06-05 16:24:29 +0000

    fix dev modus in deploment pipline

--- target/classes/db/migration/V1__create_locations.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE locations (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(100) NOT NULL,
-    latitude DOUBLE NOT NULL,
-    longitude DOUBLE NOT NULL,
-    timestamp DATETIME NOT NULL
-);
